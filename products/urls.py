e169f37545de5aecc45d9ce13f1d02a214393fe2
Shamit2105 <202301041@daiict.ac.in>
2025-01-05 14:04:17 +0530

    seller can add items via templates

--- products/urls.py
+++ products/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import ItemListView,ItemDetailView
+from .views import CategoryCreateView,SubcategoryCreateView,ItemCreateView,ItemListView,ItemDetailView
 
 urlpatterns = [
     path('', ItemListView.as_view(), name='home'),
     path('product/<int:pk>/', ItemDetailView.as_view(), name='product_detail'),
-
+    path('create_category/<int:user_id>/',CategoryCreateView.as_view(),name='create_category'),
+    path('create_subcategory/<int:user_id>/',SubcategoryCreateView.as_view(),name='create_subcategory'),
+    path('create_item/<int:user_id>/',ItemCreateView.as_view(),name='create_item'),
 ]
--- products/views.py
+++ products/views.py
@@ -1,8 +1,29 @@
+from django.urls import reverse_lazy
 from django.shortcuts import render
 from django.views.generic import ListView,TemplateView,DetailView
+from django.views.generic.edit import CreateView,FormView
 
 from .models import Category, SubCategory, Item
+from .forms import CategoryForm,SubCategoryForm,ItemForm
 # Create your views here.
+
+class CategoryCreateView(CreateView):
+    model = Category
+    template_name = 'category_create.html'
+    form_class = CategoryForm
+    success_url = reverse_lazy('home')
+
+class SubcategoryCreateView(CreateView):
+    model = SubCategory
+    template_name = 'subcategory_create.html'
+    form_class = SubCategoryForm
+    success_url = reverse_lazy('home')
+
+class ItemCreateView(CreateView):
+    model = Item
+    template_name = 'item_create.html'
+    form_class = ItemForm
+    success_url = reverse_lazy('home')
 
 class ItemListView(ListView):
     template_name = 'home.html'
